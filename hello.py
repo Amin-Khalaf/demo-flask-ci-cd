f460758d3006dc550f9227da3a26f97140868ba4
Vagrant Default User <vagrant@control>
2019-10-21 10:07:53 +0000

    fix the error

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ APP = Flask(__name__)
 @APP.route("/")
 def hello():
     """ hello function """
-   return "Hello World!"
+    return "Hello World!"
 
 if __name__ == "__main__":
     APP.run(host="0.0.0.0")
